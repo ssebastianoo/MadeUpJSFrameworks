02086cbfcf6d5ea9a3cba7fcbdb7b8eca6269e20
ssebastianoo <sebastianogirotto05@gmail.com>
2023-01-12 20:09:18 +0100

    wait 30 minutes before checking'

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ openai.api_key = config.OPENAI_API_KEY
 
 
 def tweet():
-    completion = openai.Completion.create(
-        engine="text-davinci-003", prompt="Tell me about a blazing fast javascript framework that has just been created by someone from the pop culture that doesn't relate to programming, don't use more than 280 characters", max_tokens=1000)
-    text = completion.choices[0].text.strip()
-    if text.startswith('.'):
-        text = text[1:].strip()
-    client.create_tweet(text=text)
+    try:
+        completion = openai.Completion.create(
+            engine="text-davinci-003", prompt="Tell me about a blazing fast javascript framework that has just been created by some famous person that doesn't relate to programming, don't use more than 280 characters", max_tokens=1000)
+        text = completion.choices[0].text.strip()
+        if text.startswith('.'):
+            text = text[1:].strip()
+        client.create_tweet(text=text)
+        print("Tweeted!")
+    except Exception as e:
+        print(e)
 
-
-resetted = False
 
 try:
     f = open("published.txt", "r")
@@ -30,7 +32,6 @@ except:
         f.write("0")
 
 while True:
-    print("checking...")
     if datetime.datetime.now().hour >= 19:
         f = open("published.txt", "r")
         published = f.read()
@@ -47,4 +48,4 @@ while True:
                 f.write("0")
             resetted = True
 
-    time.sleep(5)
+    time.sleep(60 * 30)
